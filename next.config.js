43343b2a223793a91cfa138ace5b3724e6f76a20
Joao Bianchi <joaobianchi@live.com>
2022-10-29 15:59:04 -0400

    :fire: removing code

--- next.config.js
+++ next.config.js
@@ -4,12 +4,6 @@ const nextConfig = {
   swcMinify: true,
   images: {
     unoptimized: true,
-  },
-  env: {
-    MAIL_SENDER: process.env.MAIL_SENDER,
-    MAIL_PASSWORD: process.env.MAIL_PASSWORD,
-    MAIL_RECIPIENT: process.env.MAIL_RECIPIENT,
-    MAIL_SERVICE: process.env.MAIL_SERVICE,
   }
 }
 
